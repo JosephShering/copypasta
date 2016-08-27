e89fea219068fda647bc3697c2deb4cd722c0c0d
Joseph Shering <reactor@js.home>
2016-08-27 00:05:47 -0400

    fix(menu): Added select all ability

--- main.js
+++ main.js
@@ -2,12 +2,33 @@ const {app, BrowserWindow, Menu} = require('electron');
 
 let mainWindow;
 
+var template = [{
+    label: "Application",
+    submenu: [
+        { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
+        { type: "separator" },
+        { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+    ]}, {
+    label: "Edit",
+    submenu: [
+        { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+        { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+        { type: "separator" },
+        { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+        { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+        { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+        { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+    ]}
+];
+
 app.on('ready', () => {
     mainWindow = new BrowserWindow({ width: 400, height: 320 });
 
     mainWindow.loadURL('file:///' + __dirname + '/index.html');
 
     mainWindow.on('closed', () => mainWindow = null);
+
+    Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 });
 
 app.on('window-all-closed', () => {
